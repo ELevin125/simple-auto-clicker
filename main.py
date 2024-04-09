35cbd44d706c0b8e90d90f9f474536be45cba6cf
Emil <emil.levin125@gmail.com>
2024-04-09 11:20:00 +0400

    added support of limited number of clicks

--- main.py
+++ main.py
@@ -11,21 +11,34 @@ class ClickMouse(threading.Thread):
         self.button = Button.left
         self.running = False
         self.program_running = True
+        self.max_clicks = tk.IntVar()
+        self.max_clicks.set(float("inf"))
+        self._current_click = 0
 
     def start_clicking(self):
         self.running = True
 
     def stop_clicking(self):
+        self._current_click = 0
         self.running = False
 
     def run(self):
         while self.program_running:
-            while self.running:
+            while self.can_run():
+                self._current_click += 1
                 mouse.press(self.button)
                 time.sleep(0.2)
                 mouse.release(self.button)
                 time.sleep(self.delay)
+            else:
+                self.stop_clicking()
             time.sleep(0.1)
+
+    def can_run(self):
+        return self.running and self._current_click < self.max_clicks.get()
+    
+    def update_max_clicks(self, new_max):
+        self.max_clicks.set(new_max)
 
     def exit(self):
         self.stop_clicking()
@@ -51,6 +64,7 @@ def update_mouse_click_type(event=None):
 def update_repeat_option(event=None):
     if repeat_var.get() == "Until Stopped":
         repeat_count_entry.config(state=tk.DISABLED)
+        click_thread.update_max_clicks(float("inf"))
     else:
         repeat_count_entry.config(state=tk.NORMAL)
 
@@ -98,9 +112,9 @@ repeat_radio1 = tk.Radiobutton(root, text="Until Stopped", variable=repeat_var, 
 repeat_radio1.grid(row=2, column=0, padx=5, pady=5)
 repeat_radio2 = tk.Radiobutton(root, text="Repeat For:", variable=repeat_var, value="Repeat For:", command=update_repeat_option)
 repeat_radio2.grid(row=3, column=0, padx=5, pady=5)
-repeat_count_entry = tk.Entry(root)
+repeat_count_entry = tk.Entry(root, textvariable=str(click_thread.max_clicks))
 repeat_count_entry.grid(row=3, column=1, padx=5, pady=5)
-repeat_count_entry.insert(tk.END, "100")
+# repeat_count_entry.insert(tk.END, "100")
 update_repeat_option()  # Disable repeat count entry initially
 
 # Start/Stop Button
